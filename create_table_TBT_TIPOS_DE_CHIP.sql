1d406f67d3ba9d3298f74a8267614ddcefdb809e
VictorSilva97 <38055800+VictorSilva97@users.noreply.github.com>
2018-06-15 13:26:25 -0300

    Delete create_table_TBT_TIPOS_DE_CHIP.sql

--- create_table_TBT_TIPOS_DE_CHIP.sql
+++
@@ -1,6 +0,0 @@
-use PROJETO_CONTROLE_DE_DISPOSITIVOS_MOVEIS;
-create table TBT_TIPOS_DE_CHIP(
-	ID_TIPO_CHIP int auto_increment not null,
-    NOME_TIPO_CHIP varchar(100) not null,
-    primary key(ID_TIPO_CHIP)
-);
