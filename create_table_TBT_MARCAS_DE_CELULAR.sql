4c9248879128a28c6abd36fd21c6304fda5e3046
VictorSilva97 <38055800+VictorSilva97@users.noreply.github.com>
2018-06-15 13:31:00 -0300

    Add files via upload

---
+++ create_table_TBT_MARCAS_DE_CELULAR.sql
@@ -0,0 +1,6 @@
+use PROJETO_CONTROLE_DE_DISPOSITIVOS_MOVEIS;
+create table TBT_MARCAS_DE_CELULAR(
+	ID_MARCA int auto_increment not null,
+    NOME_MARCA varchar(100) not null,
+    primary key(ID_MARCA)
+);
---
+++ create_table_TBT_OPERADORAS_TELEFONICAS.sql
@@ -0,0 +1,6 @@
+use PROJETO_CONTROLE_DE_DISPOSITIVOS_MOVEIS;
+create table TBT_OPERADORAS_TELEFONICAS(
+	ID_OPERADORA int auto_increment not null,
+    NOME_OPERADORA varchar(100) not null,
+    primary key(ID_OPERADORA)
+);
