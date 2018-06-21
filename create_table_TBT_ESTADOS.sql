57c91d290aa686d8e1b0a04b3b9e6beef35669e3
VictorSilva97 <38055800+VictorSilva97@users.noreply.github.com>
2018-06-21 19:33:11 -0300

    Delete create_table_TBT_ESTADOS.sql

--- create_table_TBT_ESTADOS.sql
+++
@@ -1,7 +0,0 @@
-use PROJETO_CONTROLE_DE_DISPOSITIVOS_MOVEIS;
-create table TBT_ESTADOS(
-	ID_ESTADO int auto_increment not null,
-    NOME_ESTADO varchar(100) not null,
-    SIGLA_ESTADO varchar(2) not null,
-    primary key(ID_ESTADO)
-);
