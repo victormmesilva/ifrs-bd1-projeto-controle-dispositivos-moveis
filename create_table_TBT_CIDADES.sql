68cfc208b6f69789d6f86f476305d401af0b3a14
VictorSilva97 <38055800+VictorSilva97@users.noreply.github.com>
2018-06-21 19:32:56 -0300

    Delete create_table_TBT_CIDADES.sql

--- create_table_TBT_CIDADES.sql
+++
@@ -1,7 +0,0 @@
-use PROJETO_CONTROLE_DE_DISPOSITIVOS_MOVEIS;
-create table TBT_CIDADES(
-	ID_CIDADE int auto_increment not null,
-    NOME_CIDADE varchar(100),
-    DDD int not null,
-    primary key(ID_CIDADE)
-);
