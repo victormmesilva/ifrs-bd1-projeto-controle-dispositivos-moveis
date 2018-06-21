9095b7743b591b34c1d8b9bebfdfba5378d3c63f
VictorSilva97 <38055800+VictorSilva97@users.noreply.github.com>
2018-06-21 19:32:38 -0300

    Delete create_table_TBT_BAIRROS.sql

--- create_table_TBT_BAIRROS.sql
+++
@@ -1,6 +0,0 @@
-use PROJETO_CONTROLE_DE_DISPOSITIVOS_MOVEIS;
-create table TBT_BAIRROS(
-	ID_BAIRRO int auto_increment not null,
-    NOME_BAIRRO varchar(100) not null,    
-    primary key(ID_BAIRRO)
-);
