b6d7abe9313e3c76408677021a603aa0912e3b18
VictorSilva97 <38055800+VictorSilva97@users.noreply.github.com>
2018-06-21 19:33:31 -0300

    Delete create_table_TBT_GENEROS.sql

--- create_table_TBT_GENEROS.sql
+++
@@ -1,6 +0,0 @@
-use PROJETO_CONTROLE_DE_DISPOSITIVOS_MOVEIS;
-create table TBT_GENEROS(
-	ID_GENERO int auto_increment not null,
-    NOME_GENERO varchar(50) not null,
-    primary key(ID_GENERO)
-);
