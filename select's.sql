eb03ad5f23c54e10a51c7b72046b2f17b12a35d3
VictorSilva97 <38055800+VictorSilva97@users.noreply.github.com>
2018-06-21 20:39:24 -0300

    Add files via upload

---
+++ select's.sql
@@ -0,0 +1,16 @@
+use PROJETO_CONTROLE_DE_DISPOSITIVOS_MOVEIS;
+
+select * from TBT_APARELHOS;
+select * from TBT_BAIRROS;
+select * from TBT_CARGOS;
+select * from TBT_CIDADES;
+select * from TBT_ESTADOS;
+select * from TBT_FUNCIONARIOS;
+select * from TBT_GENEROS;
+select * from TBT_LINHAS_TELEFONICAS;
+select * from TBT_MARCAS_DE_CELULAR;
+select * from TBT_MODELOS_DE_CELULAR;
+select * from TBT_OPERADORAS_TELEFONICAS;
+select * from TBT_PAISES;
+select * from TBT_RUAS;
+select * from TBT_TIPOS_DE_CHIP;
