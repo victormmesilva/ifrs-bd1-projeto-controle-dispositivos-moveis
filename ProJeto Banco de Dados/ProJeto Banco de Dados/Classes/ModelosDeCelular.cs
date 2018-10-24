2002dd02dabcba15ce949ec46b0a4a9d2ae49cb4
EDERSONMEIRA <ederson.ti.qi@gmail.com>
2018-10-24 17:45:00 -0300

    criado Class Tipo de Chip, Modelos cel., Marcas cel. ,Linhas telefonica

---
+++ ProJeto Banco de Dados/ProJeto Banco de Dados/Classes/ModelosDeCelular.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProJeto_Banco_de_Dados.Classes
+{
+    class ModelosDeCelular
+    {
+        public int ID { get; set; }
+        public String NomeModelo { get; set; }
+        public int NumeroChip { get; set; }
+        public MarcasDeCelular MarcaCelular { get; set; }
+        public TiposDeChip TipoDeChip { get; set; }
+
+
+        public ModelosDeCelular(int ID, string NomeModelo, int NumeroChip, MarcasDeCelular MarcaCelular, TiposDeChip TipoDeChip)
+        {
+            this.ID = ID;
+            this.NomeModelo = NomeModelo;
+            this.NumeroChip = NumeroChip;
+            this.MarcaCelular = MarcaCelular;
+            this.TipoDeChip = TipoDeChip;
+        }
+
+      
+
+    }
+}
