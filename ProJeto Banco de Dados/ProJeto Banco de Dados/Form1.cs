ff8b8c1bad2018210b41428ebec322973f114d35
EDERSONMEIRA <ederson.ti.qi@gmail.com>
2018-10-25 15:09:12 -0300

    Criado conexao app e BD

--- ProJeto Banco de Dados/ProJeto Banco de Dados/Form1.cs
+++ ProJeto Banco de Dados/ProJeto Banco de Dados/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ProJeto_Banco_de_Dados
 {
@@ -17,9 +18,24 @@ namespace ProJeto_Banco_de_Dados
             InitializeComponent();
         }
 
+       
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection objConexao = new MySqlConnection("Server=localhost;port=3306;UId=root;Database=PROJETO_CONTROLE_DE_DISPOSITIVOS_MOVEIS; Pwd=root");
+                objConexao.Open();
+                MessageBox.Show("conectado");
+                objConexao.Close();
+            }
+            catch {
+                MessageBox.Show("não conectado");
+            }
         }
     }
 }
