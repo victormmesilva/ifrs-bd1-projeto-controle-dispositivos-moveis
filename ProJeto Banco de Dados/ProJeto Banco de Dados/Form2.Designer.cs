82109a12dab571def68bb601707b08ed5559b2bb
EDERSONMEIRA <ederson.ti.qi@gmail.com>
2018-11-01 12:44:30 -0300

    ajuste

---
+++ ProJeto Banco de Dados/ProJeto Banco de Dados/Form2.Designer.cs
@@ -0,0 +1,275 @@
+﻿using System;
+
+namespace ProJeto_Banco_de_Dados
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtImagem = new System.Windows.Forms.TextBox();
+            this.btnInserirImagem = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtImei = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNumeroSerie = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMacAdress = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtLinha = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtFuncionario = new System.Windows.Forms.TextBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProJeto_Banco_de_Dados.Properties.Resources.logo_fotografia_1;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(214, 262);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtImagem
+            // 
+            this.txtImagem.Location = new System.Drawing.Point(24, 280);
+            this.txtImagem.Name = "txtImagem";
+            this.txtImagem.Size = new System.Drawing.Size(214, 20);
+            this.txtImagem.TabIndex = 1;
+            // 
+            // btnInserirImagem
+            // 
+            this.btnInserirImagem.Location = new System.Drawing.Point(24, 306);
+            this.btnInserirImagem.Name = "btnInserirImagem";
+            this.btnInserirImagem.Size = new System.Drawing.Size(214, 19);
+            this.btnInserirImagem.TabIndex = 2;
+            this.btnInserirImagem.Text = "Inserir Imagem";
+            this.btnInserirImagem.UseVisualStyleBackColor = true;
+            this.btnInserirImagem.Click += new System.EventHandler(this.btnInserirImagem_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(515, 327);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 3;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(267, 62);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtSenha.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(264, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Senha";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(267, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Imei";
+            // 
+            // txtImei
+            // 
+            this.txtImei.Location = new System.Drawing.Point(267, 22);
+            this.txtImei.Name = "txtImei";
+            this.txtImei.Size = new System.Drawing.Size(423, 20);
+            this.txtImei.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(371, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Numero de Serie";
+            // 
+            // txtNumeroSerie
+            // 
+            this.txtNumeroSerie.Location = new System.Drawing.Point(374, 63);
+            this.txtNumeroSerie.Name = "txtNumeroSerie";
+            this.txtNumeroSerie.Size = new System.Drawing.Size(316, 20);
+            this.txtNumeroSerie.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(265, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "MacAdress";
+            // 
+            // txtMacAdress
+            // 
+            this.txtMacAdress.Location = new System.Drawing.Point(268, 104);
+            this.txtMacAdress.Name = "txtMacAdress";
+            this.txtMacAdress.Size = new System.Drawing.Size(169, 20);
+            this.txtMacAdress.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(440, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Modelo";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(443, 104);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(247, 20);
+            this.txtModelo.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(267, 128);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Linha";
+            // 
+            // txtLinha
+            // 
+            this.txtLinha.Location = new System.Drawing.Point(269, 144);
+            this.txtLinha.Name = "txtLinha";
+            this.txtLinha.Size = new System.Drawing.Size(100, 20);
+            this.txtLinha.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(372, 127);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Funcionario";
+            // 
+            // txtFuncionario
+            // 
+            this.txtFuncionario.Location = new System.Drawing.Point(375, 143);
+            this.txtFuncionario.Name = "txtFuncionario";
+            this.txtFuncionario.Size = new System.Drawing.Size(100, 20);
+            this.txtFuncionario.TabIndex = 19;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(597, 327);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 20;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(702, 362);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.txtFuncionario);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtLinha);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtModelo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMacAdress);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNumeroSerie);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtImei);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnInserirImagem);
+            this.Controls.Add(this.txtImagem);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+       
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtImagem;
+        private System.Windows.Forms.Button btnInserirImagem;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtImei;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNumeroSerie;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMacAdress;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtLinha;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtFuncionario;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
---
+++ ProJeto Banco de Dados/ProJeto Banco de Dados/Form2.cs
@@ -0,0 +1,114 @@
+﻿using MySql.Data.MySqlClient;
+using ProJeto_Banco_de_Dados.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProJeto_Banco_de_Dados
+{
+    public partial class Form2 : Form
+    {
+        //private string _conection = "Server=localhost;port=3306;UId=root;Database=PROJETO_CONTROLE_DE_DISPOSITIVOS_MOVEIS; Pwd=root";
+        //MySqlConnection objConexao = null;
+        //MySqlCommand comando_insert = null;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInserirImagem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "JPEG Files(*.jpg)|*.jpg|PNG Files(*.png)|*.png|AllFiles(*.*)|*.*";
+
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                string foto = dialog.FileName.ToString();
+                txtImagem.Text = foto;
+                pictureBox1.ImageLocation = foto;
+
+            }
+
+
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+
+
+            byte[] imagem_byti= null;
+            FileStream fstream = new FileStream(this.txtImagem.Text, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fstream);
+            imagem_byti = br.ReadBytes((int)fstream.Length);
+
+
+
+            Conexao con = new Conexao();    // criando objeto de conexao
+            MySqlConnection conectar = con.ObjConexao(); // chamando o metodo de conexao e atribuindo no conectar o retorno do metodo
+
+            string insert = "INSERT INTO tbt_aparelhos (SENHA_APARELHO,IMEI,NUMERO_DE_SERIE,MAC_ADDRESS,FK_ID_MODELO,FK_ID_LINHA,FK_ID_FUNCIONARIO,FOTO) VALUES (@SENHA_APARELHO,@IMEI,@NUMERO_DE_SERIE,@MAC_ADDRESS,@FK_ID_MODELO,@FK_ID_LINHA,@FK_ID_FUNCIONARIO,@FOTO)";
+
+            MySqlCommand comando_insert = con.comando_banco(insert, conectar); //chamando metodo comando_consultar do mysql e mandando o camando e a nonexao
+
+            //objConexao = new MySqlConnection(_conection);
+            //comando_insert = new MySqlCommand(insert, objConexao);
+            MySqlDataReader meu_reader;
+            
+            try
+            {
+                con.open(conectar);// abrindo conexao
+                
+
+
+                comando_insert.Parameters.Add(new MySqlParameter("@SENHA_APARELHO", txtSenha.Text));
+                comando_insert.Parameters.Add(new MySqlParameter("@IMEI", txtImei.Text));
+                comando_insert.Parameters.Add(new MySqlParameter("@NUMERO_DE_SERIE", txtNumeroSerie.Text));
+                comando_insert.Parameters.Add(new MySqlParameter("@MAC_ADDRESS", txtMacAdress.Text));
+                comando_insert.Parameters.Add(new MySqlParameter("@FK_ID_MODELO", txtModelo.Text));
+                comando_insert.Parameters.Add(new MySqlParameter("@FK_ID_LINHA", txtLinha.Text));
+                comando_insert.Parameters.Add(new MySqlParameter("@FK_ID_FUNCIONARIO", txtFuncionario.Text));
+                comando_insert.Parameters.Add(new MySqlParameter("@FOTO", imagem_byti));
+
+               // comando_insert.Parameters.Add(new MySqlParameter("@nome_Aparelho", txtNome.Text));
+               // var rowsAffecteds = comando_insert.ExecuteNonQuery();
+
+                meu_reader = comando_insert.ExecuteReader();
+
+                MessageBox.Show("Cadastro Salvo");
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.close(conectar);
+
+            }
+
+
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Form2.ActiveForm.Close();
+        }
+    }
+}
