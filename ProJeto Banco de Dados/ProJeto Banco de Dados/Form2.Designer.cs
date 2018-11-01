b64dda127369a66a60b87cf422e2764b0ee91f65
EDERSONMEIRA <ederson.ti.qi@gmail.com>
2018-11-01 08:37:50 -0300

    ajuste tela cadastro celular

--- ProJeto Banco de Dados/ProJeto Banco de Dados/Form2.Designer.cs
+++ ProJeto Banco de Dados/ProJeto Banco de Dados/Form2.Designer.cs
@@ -28,12 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.txtImagem = new System.Windows.Forms.TextBox();
             this.btnInserirImagem = new System.Windows.Forms.Button();
             this.btnSalvar = new System.Windows.Forms.Button();
-            this.txtNome = new System.Windows.Forms.TextBox();
-            this.lbNome = new System.Windows.Forms.Label();
             this.txtSenha = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -48,30 +47,32 @@
             this.txtLinha = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.txtFuncionario = new System.Windows.Forms.TextBox();
+            this.btnFechar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(24, 35);
+            this.pictureBox1.Image = global::ProJeto_Banco_de_Dados.Properties.Resources.logo_fotografia_1;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(146, 157);
+            this.pictureBox1.Size = new System.Drawing.Size(214, 262);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // txtImagem
             // 
-            this.txtImagem.Location = new System.Drawing.Point(24, 198);
+            this.txtImagem.Location = new System.Drawing.Point(24, 280);
             this.txtImagem.Name = "txtImagem";
-            this.txtImagem.Size = new System.Drawing.Size(146, 20);
+            this.txtImagem.Size = new System.Drawing.Size(214, 20);
             this.txtImagem.TabIndex = 1;
             // 
             // btnInserirImagem
             // 
-            this.btnInserirImagem.Location = new System.Drawing.Point(24, 224);
+            this.btnInserirImagem.Location = new System.Drawing.Point(24, 306);
             this.btnInserirImagem.Name = "btnInserirImagem";
-            this.btnInserirImagem.Size = new System.Drawing.Size(146, 19);
+            this.btnInserirImagem.Size = new System.Drawing.Size(214, 19);
             this.btnInserirImagem.TabIndex = 2;
             this.btnInserirImagem.Text = "Inserir Imagem";
             this.btnInserirImagem.UseVisualStyleBackColor = true;
@@ -79,7 +80,7 @@
             // 
             // btnSalvar
             // 
-            this.btnSalvar.Location = new System.Drawing.Point(325, 222);
+            this.btnSalvar.Location = new System.Drawing.Point(515, 327);
             this.btnSalvar.Name = "btnSalvar";
             this.btnSalvar.Size = new System.Drawing.Size(75, 23);
             this.btnSalvar.TabIndex = 3;
@@ -87,25 +88,9 @@
             this.btnSalvar.UseVisualStyleBackColor = true;
             this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
-            // txtNome
-            // 
-            this.txtNome.Location = new System.Drawing.Point(194, 35);
-            this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(100, 20);
-            this.txtNome.TabIndex = 4;
-            // 
-            // lbNome
-            // 
-            this.lbNome.AutoSize = true;
-            this.lbNome.Location = new System.Drawing.Point(194, 13);
-            this.lbNome.Name = "lbNome";
-            this.lbNome.Size = new System.Drawing.Size(80, 13);
-            this.lbNome.TabIndex = 5;
-            this.lbNome.Text = "Nome Aparelho";
-            // 
             // txtSenha
             // 
-            this.txtSenha.Location = new System.Drawing.Point(300, 35);
+            this.txtSenha.Location = new System.Drawing.Point(267, 62);
             this.txtSenha.Name = "txtSenha";
             this.txtSenha.Size = new System.Drawing.Size(100, 20);
             this.txtSenha.TabIndex = 6;
@@ -113,7 +98,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(297, 13);
+            this.label1.Location = new System.Drawing.Point(264, 46);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 7;
@@ -122,7 +107,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(194, 62);
+            this.label2.Location = new System.Drawing.Point(267, 5);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(26, 13);
             this.label2.TabIndex = 8;
@@ -130,15 +115,15 @@
             // 
             // txtImei
             // 
-            this.txtImei.Location = new System.Drawing.Point(194, 79);
+            this.txtImei.Location = new System.Drawing.Point(267, 22);
             this.txtImei.Name = "txtImei";
-            this.txtImei.Size = new System.Drawing.Size(103, 20);
+            this.txtImei.Size = new System.Drawing.Size(423, 20);
             this.txtImei.TabIndex = 9;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(297, 62);
+            this.label3.Location = new System.Drawing.Point(371, 46);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(86, 13);
             this.label3.TabIndex = 10;
@@ -146,15 +131,15 @@
             // 
             // txtNumeroSerie
             // 
-            this.txtNumeroSerie.Location = new System.Drawing.Point(300, 79);
+            this.txtNumeroSerie.Location = new System.Drawing.Point(374, 63);
             this.txtNumeroSerie.Name = "txtNumeroSerie";
-            this.txtNumeroSerie.Size = new System.Drawing.Size(100, 20);
+            this.txtNumeroSerie.Size = new System.Drawing.Size(316, 20);
             this.txtNumeroSerie.TabIndex = 11;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(191, 106);
+            this.label4.Location = new System.Drawing.Point(265, 88);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(60, 13);
             this.label4.TabIndex = 12;
@@ -162,15 +147,15 @@
             // 
             // txtMacAdress
             // 
-            this.txtMacAdress.Location = new System.Drawing.Point(194, 122);
+            this.txtMacAdress.Location = new System.Drawing.Point(268, 104);
             this.txtMacAdress.Name = "txtMacAdress";
-            this.txtMacAdress.Size = new System.Drawing.Size(100, 20);
+            this.txtMacAdress.Size = new System.Drawing.Size(169, 20);
             this.txtMacAdress.TabIndex = 13;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(297, 106);
+            this.label5.Location = new System.Drawing.Point(440, 86);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(42, 13);
             this.label5.TabIndex = 14;
@@ -179,15 +164,15 @@
             // 
             // txtModelo
             // 
-            this.txtModelo.Location = new System.Drawing.Point(300, 122);
+            this.txtModelo.Location = new System.Drawing.Point(443, 104);
             this.txtModelo.Name = "txtModelo";
-            this.txtModelo.Size = new System.Drawing.Size(100, 20);
+            this.txtModelo.Size = new System.Drawing.Size(247, 20);
             this.txtModelo.TabIndex = 15;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(194, 152);
+            this.label6.Location = new System.Drawing.Point(267, 128);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(33, 13);
             this.label6.TabIndex = 16;
@@ -195,7 +180,7 @@
             // 
             // txtLinha
             // 
-            this.txtLinha.Location = new System.Drawing.Point(194, 169);
+            this.txtLinha.Location = new System.Drawing.Point(269, 144);
             this.txtLinha.Name = "txtLinha";
             this.txtLinha.Size = new System.Drawing.Size(100, 20);
             this.txtLinha.TabIndex = 17;
@@ -203,7 +188,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(297, 152);
+            this.label7.Location = new System.Drawing.Point(372, 127);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(62, 13);
             this.label7.TabIndex = 18;
@@ -211,18 +196,28 @@
             // 
             // txtFuncionario
             // 
-            this.txtFuncionario.Location = new System.Drawing.Point(300, 168);
+            this.txtFuncionario.Location = new System.Drawing.Point(375, 143);
             this.txtFuncionario.Name = "txtFuncionario";
             this.txtFuncionario.Size = new System.Drawing.Size(100, 20);
             this.txtFuncionario.TabIndex = 19;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(597, 327);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 20;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::ProJeto_Banco_de_Dados.Properties.Resources.background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(458, 272);
+            this.ClientSize = new System.Drawing.Size(702, 362);
+            this.Controls.Add(this.btnFechar);
             this.Controls.Add(this.txtFuncionario);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.txtLinha);
@@ -237,13 +232,13 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtSenha);
-            this.Controls.Add(this.lbNome);
-            this.Controls.Add(this.txtNome);
             this.Controls.Add(this.btnSalvar);
             this.Controls.Add(this.btnInserirImagem);
             this.Controls.Add(this.txtImagem);
             this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -257,8 +252,6 @@
         private System.Windows.Forms.TextBox txtImagem;
         private System.Windows.Forms.Button btnInserirImagem;
         private System.Windows.Forms.Button btnSalvar;
-        private System.Windows.Forms.TextBox txtNome;
-        private System.Windows.Forms.Label lbNome;
         private System.Windows.Forms.TextBox txtSenha;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
@@ -273,5 +266,6 @@
         private System.Windows.Forms.TextBox txtLinha;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox txtFuncionario;
+        private System.Windows.Forms.Button btnFechar;
     }
 }
