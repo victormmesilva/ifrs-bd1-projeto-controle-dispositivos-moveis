3181454a6d99f5974f174fa39d495bc299a1d804
EDERSONMEIRA <ederson.ti.qi@gmail.com>
2018-11-01 12:35:28 -0300

    renomear form

---
+++ ProJeto Banco de Dados/ProJeto Banco de Dados/FormPrincipal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ProJeto_Banco_de_Dados
+{
+    public partial class FormPrincipal : Form
+    {
+        
+
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+
+        private void casdastroCelularToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cadastroToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void novoCadastroToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CadastroCelular cadastro_celular = new CadastroCelular();
+            cadastro_celular.Show();
+            
+        }
+
+        private void consultaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ConsultarCelular consultar_celular = new ConsultarCelular();
+            consultar_celular.Show();
+        }
+
+        private void dgView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void consultarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ConsultarFuncionario consulta_funcionario = new ConsultarFuncionario();
+            consulta_funcionario.Show();
+        }
+    }
+}
