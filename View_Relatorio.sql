2828e241b20426e364b0acdb85d031313f6e138e
DennerEuzebio <42979433+DennerEuzebio@users.noreply.github.com>
2018-11-27 21:45:17 -0200

    Update View_Relatorio.sql

--- View_Relatorio.sql
+++ View_Relatorio.sql
@@ -2,30 +2,40 @@
 use PROJETO_CONTROLE_DE_DISPOSITIVOS_MOVEIS;
 
 create view view_relatorio as 
-	select 'QT do Funcionário'	AS 'RELATORIO',
-	COUNT(*) from TBT_FUNCIONARIOS union
-	select 	'QT do aparelhos',
+	select 'QT DOS APARELHOS'	AS 'RELATORIO',
 	COUNT(*) from TBT_APARELHOS  union
-	select 	'QT PAISES',
-	COUNT(*) from TBT_PAISES union
-	select 	'QT CIDADE',
-	COUNT(*) from TBT_CIDADES union
-	select 	'QT ESTADOS',
-	COUNT(*) from TBT_ESTADOS  union
-	select 	'QT LINHAS',
-	COUNT(*) from TBT_LINHAS_TELEFONICAS  union
-	select 	'QT BAIRROS',
-	COUNT(*) from TBT_BAIRROS  union
-	select 	'QT CARGOS',
+    select 	'QT DOS BAIRROS',
+	COUNT(*) from TBT_BAIRROS union
+    select 	'QT DOS CARGOS',
 	COUNT(*) from TBT_CARGOS union
-	select 	'QT MARCAS DE CELULAR',
+    select 	'QT DAS CIDADES',
+	COUNT(*) from TBT_CIDADES union
+    select 	'QT DOS ESTADOS',
+	COUNT(*) from TBT_ESTADOS union
+    select 	'QT DA FILA DE ESPERA',
+	COUNT(*) from TBT_FILA_DE_ESPERA union
+    select 	'QT DOS FUNCIONARIOS TOTAIS',
+	COUNT(*) from TBT_FUNCIONARIOS union
+    select 	'QT DOS FUNCIONARIOS COM APARELHO',
+	COUNT(*) from TBT_FUNCIONARIOS_APARELHOS union
+    select 	'QT DOS GENEROS',
+	COUNT(*) from TBT_GENEROS union
+    select 	'QT DAS LINHAS TELEFONICAS',
+	COUNT(*) from TBT_LINHAS_TELEFONICAS union
+    select 	'QT DAS MARCAS DE CELULAR',
 	COUNT(*) from TBT_MARCAS_DE_CELULAR union
-	select 	'QT MODELOS',
+    select 	'QT DOS MODELOS DE CELULAR',
 	COUNT(*) from TBT_MODELOS_DE_CELULAR union
-	select 	'QT OPERADORAS',
+    select 	'QT DAS OPERADOS TELEFONICAS',
 	COUNT(*) from TBT_OPERADORAS_TELEFONICAS union
-	select 	'QT TIPOS DE CHIP',
-	COUNT(*) from TBT_TIPOS_DE_CHIP  
+    select 	'QT DOS PAISES',
+	COUNT(*) from TBT_PAISES union
+    select 	'QT DAS RUAS',
+	COUNT(*) from TBT_RUAS union
+    select 	'QT TELEFONES DOS FUNCIONARIOS',
+	COUNT(*) from TBT_TELEFONES_FUNCIONARIOS union
+    select 	'QT DOS TIPOS DE CHIP',
+	COUNT(*) from TBT_TIPOS_DE_CHIP
   
 
 
